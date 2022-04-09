7ae5c8657fc23745f5ecbab71ed23719d7f590e8
subbuinti <subbuinti1@gmail.com>
2022-04-09 20:22:27 +0530

    first commit

--- factorial.js
+++ factorial.js
@@ -1,12 +1,22 @@
-function factorial(number) {
-  let result = 1;
+// function factorial(number) {
+//   let result = 1;
 
-  for (let i = 2; i <= number; i++) {
-    // console.log(result);
-    result = result * i;
-  }
+//   for (let i = 2; i <= number; i++) {
+//     // console.log(result);
+//     result = result * i;
+//   }
 
-  return result;
+//   return result;
+// }
+
+// console.log(factorial(100));
+
+let rain = 10;
+let animal = 0;
+
+while (rain < 13 || animal <= 2) {
+  console.log(animal);
+  rain++;
+  animal += 2;
 }
-
-console.log(factorial(100));
+console.log(animal);
