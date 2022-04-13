f0dea5fe1423f372867b47e505266ac54c44aa9a
subbuinti <subbuinti1@gmail.com>
2022-04-13 22:34:12 +0530

    first commit

---
+++ lcmGcd.js
@@ -0,0 +1,18 @@
+function CountPairs(a, n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j < n; j++) {
+      let lcm = (i, j) => {
+        let gcd = gcd(i, j);
+      };
+      return (i * j) / gcd;
+    }
+  }
+}
+function gcd(i, j) {
+  if (j === 0) return i;
+  /* else */ return gcd(j, i % j);
+}
+
+let arr = [2, 3, 4];
+let n = arr.length;
+console.log(CountPairs(arr, n));
